3214aff0078b74c477992e4e69903a163441403f
mtrsuraj <surajprintf33@gmail.com>
2023-11-02 17:40:14 +0530

    first commit

---
+++ src/test/java/com/tech/info/EmployeeProductManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tech.info;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeProductManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
